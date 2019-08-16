ce58fc5b1e347ba29d55b23579e499399c5b68e4
M Banks <montensia1@outlook.com>
2019-08-16 13:43:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 // Write your code here!
 
 var element = document.createElement('div');
+
+document.body.appendChild(element);
+
+var ul = document.createElement('ul');
+
+for(let i = 1; i < 3; i++){
+  let li = document.createElement('li');
+  li.innerHTML = (i).toString();
+  ul.appendChild(li);
+}
