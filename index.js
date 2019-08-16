2dd02d90d3076856d8fac4a882380a801b471e38
M Banks <montensia1@outlook.com>
2019-08-16 14:02:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,31 +1,31 @@
 // Write your code here!
 
-var element = document.createElement('div');
+// var element = document.createElement('div');
 
-document.body.appendChild(element);
+// document.body.appendChild(element);
 
-var ul = document.createElement('ul');
+// var ul = document.createElement('ul');
 
-for(let i = 1; i <= 3; i++){
-  let li = document.createElement('li');
-  li.innerHTML = (i).toString();
-  ul.appendChild(li);
-}
+// for(let i = 1; i <= 3; i++){
+//   let li = document.createElement('li');
+//   li.innerHTML = (i).toString();
+//   ul.appendChild(li);
+// }
 
-var para = document.createElement("p#greeting");
-element.appendChild(0);
+// var para = document.createElement("p#greeting");
+// element.appendChild(0);
 
-let element2 = document.querySelector("p#greeting");
+// let element2 = document.querySelector("p#greeting");
 
-element2.innerHTML = "Hello, DOM!";
+// element2.innerHTML = "Hello, DOM!";
 
-let header = document.getElementById("div#header");
+// let header = document.getElementById("div#header");
 
-header.innerHTML = "<h1>Poodles!</h1><h3>An Essay into the Pom-Pom as Aesthetic Reconfiguration of the Other from a post-Frankfurt School Appropriationist Perspective</h3><p><em>By: Byron Q. Poodle, Esq., BA.</em></p>";
-element.style.backgroundColor = '#27647B';
-element.style.textAlign = 'center';
-ul.style.textAlign = 'left';
+// header.innerHTML = "<h1>Poodles!</h1><h3>An Essay into the Pom-Pom as Aesthetic Reconfiguration of the Other from a post-Frankfurt School Appropriationist Perspective</h3><p><em>By: Byron Q. Poodle, Esq., BA.</em></p>";
+// element.style.backgroundColor = '#27647B';
+// element.style.textAlign = 'center';
+// ul.style.textAlign = 'left';
 
-element.className = "dog";
-element.className = "pet-listing dog";
+// element.className = "dog";
+// element.className = "pet-listing dog";
 
