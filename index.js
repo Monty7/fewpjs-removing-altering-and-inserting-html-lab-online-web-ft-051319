82c71c3f37b6ea4f59ab183054f77d3968dd787c
M Banks <montensia1@outlook.com>
2019-08-16 13:49:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,8 +6,14 @@ document.body.appendChild(element);
 
 var ul = document.createElement('ul');
 
-for(let i = 1; i < 3; i++){
+for(let i = 1; i <= 3; i++){
   let li = document.createElement('li');
   li.innerHTML = (i).toString();
   ul.appendChild(li);
 }
+
+element.appendChild(0);
+
+let element = document.querySelector("p#greeting");
+
+element.innerHTML = "Hello, DOM!"
