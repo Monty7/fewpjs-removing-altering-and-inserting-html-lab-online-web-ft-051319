7c3cb7fbc00b0503b58ae971deb9690598815720
M Banks <montensia1@outlook.com>
2019-08-16 14:16:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -32,3 +32,4 @@ document.querySelector("main#main").remove();
 let newHeader = document.createElement('h1');
 //newHeader.setAttribute("id", "victory");
 newHeader.id = "victory";
+newHeader.innerHTML("YOUR-NAME is the champion")
