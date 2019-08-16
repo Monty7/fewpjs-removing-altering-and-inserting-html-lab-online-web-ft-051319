a35eaa4384907f54ede6b6b4aefbe39f2f43a1f9
M Banks <montensia1@outlook.com>
2019-08-16 14:11:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -29,5 +29,5 @@
 // element.className = "dog";
 // element.className = "pet-listing dog";
 document.querySelector("main#main").remove();
-let newHeader = document.querySelector('h1#victory');
+let newHeader = document.querySelector('h1');
 
